379827c333e22f059159c845bdc440ca546c14c3
mxngls <Maximilian.e.hoenig@gmail.com>
2024-04-17 23:12:20 +0900

    (WIP) Create index.html with new build script

--- build.sh
+++ build.sh
@@ -2,9 +2,12 @@
 
 set -o errexit
 set -o nounset
-# set -o pipefail
+set -o pipefail
 
+# constants
 SOURCE='src'
+MD_CONVERT='pandoc'
+TITLE="Max Website"
 
 # Metadata format:
 # title
@@ -12,23 +15,86 @@ SOURCE='src'
 # created_at
 # updated_at
 
-IFS=' '
+# redefine IFS to split on tabs
+IFS='	'
+
+# tabs as field separator
 meta_tsv() {
   for f in "$SOURCE"/*.md; do
     if [[ "$f" =~ index.md ]]; then continue; fi
 
-    title="$(awk -v RS= '
+    read -r title subtitle < <(
+      awk -v RS= '
       # Parse title
-      NR == 1 { title=$0; sub(/#[[:space:]]+/,"",title); }
+      NR == 1 { 
+        sub(/#[[:space:]]+/,""); 
+        title=$0; 
+      }
 
       # Parse subtitle
-      NR == 2 { sub(/\n/,"\\n"); subtitle=$0; print title "\t" subtitle; exit;}' "$f")"
+      NR == 2 { 
+        sub(/\n/,"\\n"); 
+        subtitle=$0; 
+        print title "\t" subtitle; 
+        exit;
+      }' "$f"
+    )
 
-    created=$(git log --pretty='format:%aI' "$f" 2> /dev/null | tail -1)
-    updated=$(git log --pretty='format:%aI' "$f" 2> /dev/null | head -1)
+    read -r created updated < <(git log \
+      --pretty='format:%aI' "$f" 2> /dev/null |
+      awk '
+          NR==1 { created=$0 }
+          END{updated=$0; print created "\t" updated;}
+      ')
 
-    printf '%s\t%s\t%s\t%s\n' "$f" "${title:="No Title"}" "${created:="draft"}" "${updated:="draft"}"
+    printf '%s\t%s\t%s\t%s\t%s\n' \
+      "$f" \
+      "${title:="No title"}" \
+      "${subtitle:="No subtitle"}" \
+      "${updated:="draft"}" \
+      "${created:="draft"}"
   done
 }
 
-meta_tsv | sort -r -t "\t" -k 3 > test.csv
+index_html() {
+
+  content="$($MD_CONVERT src/index.md)"
+
+  while read -r f title subtitle created updated; do
+    f="$(basename "$f")"
+    ref="${f/%.md/.html}"
+    created=$(awk '{sub(/T.*/,""); print $0}' <<< "$created")
+    posts+=$(printf "
+    <li>
+        <span>%s</span> \&nbsp; <a href=docs/%s>%s</a>
+    </li>\n" "$created" "$ref" "$title")
+  done < "$1"
+
+  content+="$(printf "
+    <ol style=\"list-style: none; padding: 0;\">
+      %s
+    </ol>" "$posts"
+  )"
+
+  # Read input as arguments to avoid escaping newlines
+  awk '
+    BEGIN {
+      title=ARGV[1]; 
+      content=ARGV[2]; 
+      ARGV[1]=""; 
+      ARGV[2]="";
+    }
+
+    /{{TITLE}}/ { sub(/{{TITLE}}/,title); }
+
+    /{{CONTENT}}/ { sub(/{{CONTENT}}/,content); }
+
+    { print $0; }' \
+    "$TITLE" \
+    "$content" \
+    'header.html'
+}
+
+meta_tsv | sort -r -t "\t" -k 4 > index.tsv
+
+index_html index.tsv > index_test.html
